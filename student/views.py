2a4766dcfc78e09c5248a7eb4135f6e510da8771
esatyasar <esatyasar91@gmail.com>
2021-10-14 21:24:43 +0300

    added models and view

--- student/views.py
+++ student/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'student/index.html')
+
+def student_page(request):
+    return render(request, 'student/student.html')
 # Create your views here.
